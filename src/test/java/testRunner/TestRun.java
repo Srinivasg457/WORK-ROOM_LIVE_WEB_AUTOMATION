d7bda33eae2910e6f41eae823a5d03bed1257912
Srinivasg457 <srinivasg457@gmail.com>
2025-07-07 17:14:38 +0530

    old script

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/Features/workroomCreateEmployee.feature",
+        features = "src/test/java/Features",
         //"src/test/java/Features/workroomCreateEmployee.feature",
         //src/test/java/Features/workroomlogin.feature
         glue = {"stepDefinations", "hooks"},
